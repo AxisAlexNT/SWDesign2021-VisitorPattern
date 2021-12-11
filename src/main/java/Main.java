b3331adea8111ae7a608b9ec7fc24b4040e93e3d
Alexander <ntwwwnt@gmail.com>
2021-12-12 01:37:42 +0300

    Implemented PrintVisitor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import ex.ApplicationRuntimeException;
 import lexer.Tokenizer;
 import lexer.tokens.NumberToken;
 import lexer.tokens.Token;
 import org.jetbrains.annotations.NotNull;
 import visitors.CalcVisitor;
 import visitors.ParserVisitor;
+import visitors.PrintVisitor;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
@@ -16,13 +18,22 @@ public class Main {
         try (final @NotNull Scanner inputScanner = new Scanner(System.in)) {
             expressionString = inputScanner.nextLine();
         }
-        final @NotNull Tokenizer tokenizer = new Tokenizer(new ByteArrayInputStream(expressionString.getBytes(StandardCharsets.UTF_8)));
-        final List<Token> sourceTokens = tokenizer.getTokens();
-        final ParserVisitor parserVisitor = new ParserVisitor();
-        sourceTokens.forEach(parserVisitor::visit);
-        final List<Token> postfixTokens = parserVisitor.getOutputExpression();
-        final CalcVisitor calcVisitor = new CalcVisitor();
-        postfixTokens.forEach(calcVisitor::visit);
-        final NumberToken calcResult = calcVisitor.getResult();
+        try {
+            final @NotNull Tokenizer tokenizer = new Tokenizer(new ByteArrayInputStream(expressionString.getBytes(StandardCharsets.UTF_8)));
+            final List<Token> sourceTokens = tokenizer.getTokens();
+            final ParserVisitor parserVisitor = new ParserVisitor();
+            sourceTokens.forEach(parserVisitor::visit);
+            final List<Token> postfixTokens = parserVisitor.getOutputExpression();
+            final PrintVisitor printVisitor = new PrintVisitor();
+            postfixTokens.forEach(printVisitor::visit);
+            System.out.println(printVisitor.getVisitResult());
+            final CalcVisitor calcVisitor = new CalcVisitor();
+            postfixTokens.forEach(calcVisitor::visit);
+            final NumberToken calcResult = calcVisitor.getResult();
+            System.out.println(calcResult.getValue());
+        } catch (final ApplicationRuntimeException e) {
+            System.out.printf("An exception occurred during expression evaluation: %s", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/visitors/PrintVisitor.java
+++ src/main/java/visitors/PrintVisitor.java
@@ -1,23 +1,40 @@
 package visitors;
 
+import ex.ApplicationRuntimeException;
 import lexer.tokens.Brace;
 import lexer.tokens.NumberToken;
 import lexer.tokens.Operation;
+import lombok.NonNull;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+
 public class PrintVisitor implements TokenVisitor {
+    private final @NotNull @NonNull StringBuilder visitBuilder = new StringBuilder();
+
     @Override
-    public void visit(@NotNull NumberToken token) {
-        throw new UnsupportedOperationException("TODO");
+    public void visit(final @NotNull NumberToken token) {
+        visitBuilder.append("NUMBER(").append(token.getValue()).append(")").append(' ');
     }
 
     @Override
-    public void visit(@NotNull Brace token) {
-        throw new UnsupportedOperationException("TODO");
+    public void visit(final @NotNull Brace token) {
+        if (token instanceof Brace.LeftBrace) {
+            visitBuilder.append("LEFT");
+        } else if (token instanceof Brace.RightBrace) {
+            visitBuilder.append("RIGHT");
+        } else {
+            throw new ApplicationRuntimeException("Unknown bracket type");
+        }
+        visitBuilder.append(' ');
     }
 
     @Override
-    public void visit(@NotNull Operation token) {
-        throw new UnsupportedOperationException("TODO");
+    public void visit(final @NotNull Operation token) {
+        visitBuilder.append(token.getClass().getSimpleName().toUpperCase(Locale.US)).append(' ');
+    }
+
+    public String getVisitResult() {
+        return visitBuilder.toString();
     }
 }
